918c2b93d6841cbf37edc419f93bdce0c299f1ae
yogithesymbian <yogirenbox33@gmail.com>
2019-05-23 00:07:47 -0400

    inital-update-function

--- pymath-dsp-yogi.py
+++ pymath-dsp-yogi.py
@@ -27,10 +27,10 @@ __source__ = "https://github.com/yogithesymbian/python-matlab-data-signal"
 __info__ = "URL scodeid"
 
 # math lib
-import matplotlib.pyplot as plt
 import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
 import scipy.fftpack  # for fourier lib
-import scipy.integrate
 import math
 # end of math lib
 import os
@@ -74,7 +74,7 @@ def bannerProgram():
     pyogclr.print_pass('.\n. \tBagaimana bentuk X(n) Baru Jika')
     pyogclr.print_fail('. \t\t\t 5. -x(n-3)')
     pyogclr.print_fail('. \t\t\t 6. -3x(n)')
-    pyogclr.print_fail('. \t\t\t 7.  x(n)-X(n-3)\n')
+    pyogclr.print_fail('. \t\t\t 7.  x(n)-x(n-3)\n')
     pyogclr.print_pass('. \tCarilah x(n)')
     pyogclr.print_fail('. \t\t\t 8. Transformasi Fourier ')
     pyogclr.print_fail('. \t\t\t 9. Transformasi Z ')
@@ -90,14 +90,19 @@ def bannerProgram():
     if ioMenu == 'dspolnes':
         ioGrafis()
     elif ioMenu == '1':
+        # clearscreen
         yogiClear()
+        # banner
         banner()
+        # function
         tabularForm()
     elif ioMenu == '2':
         yogiClear()
         banner()
         bentukFxn()
     elif ioMenu == '3':
+        yogiClear()
+        banner()
         yoGrafis()
     elif ioMenu == '4':
         yogiClear()
@@ -107,8 +112,25 @@ def bannerProgram():
         yogiClear()
         banner()
         nomorLima()
+    elif ioMenu == '6':
+        yogiClear()
+        banner()
+        nomorEnam()
+    elif ioMenu == '7':
+        yogiClear()
+        banner()
+        nomorTujuh()
     elif ioMenu == '8':
+        yogiClear()
+        banner()
         yoFourier()
+        # y/n [against|exit]
+        yogiAsk()
+    elif ioMenu == '9':
+        yogiClear()
+        banner()
+        yoTransz()
+        yogiAsk()
     else:
         pyogclr.print_bold('still development choose other menu')
 
@@ -124,31 +146,123 @@ def yogiClear():
     os.system('clear')
 
 
-# initial condition
-y0 = 5
+def nomorLima():
+    pyogclr.print_pass('            THIS A BENTUK -x(n-3) :')
+    print('From : x = ', x)
+    print('       n = ', n)
+    pyogclr.print_pass('opening figure . . .')
+    x1 = -x[0]
+    x2 = -x[1]
+    x3 = -x[2]
+    x4 = -x[3]
+    x5 = -x[4]
+    x6 = -x[5]
+    xberubah = x1, x2, x3, x4, x5, x6
+    n1 = n[0] - 3
+    n2 = n[1] - 3
+    n3 = n[2] - 3
+    n4 = n[3] - 3
+    n5 = n[4] - 3
+    n6 = n[5] - 3
+    nberubah = n1, n2, n3, n4, n5, n6
+    # xberubah = -x
+    # nberubah = n - 3
+    grafis(nberubah, xberubah)
 
-# time points
-t = np.linspace(0, 20)
 
-# function that return dy/dt
+def nomorEnam():
+    pyogclr.print_pass('            THIS A BENTUK -3x(n) :')
+    print('From : x = ', x)
+    print('       n = ', n)
+    pyogclr.print_pass('opening figure . . .')
+    minusnya = -3
+    x1 = minusnya * x[0]
+    x2 = minusnya * x[1]
+    x3 = minusnya * x[2]
+    x4 = minusnya * x[3]
+    x5 = minusnya * x[4]
+    x6 = minusnya * x[5]
+    xberubah = x1, x2, x3, x4, x5, x6
+    # xberubah = -x
+    # nberubah = n - 3
+    grafis(n, xberubah)
 
 
+def nomorTujuh():
+    pyogclr.print_pass('            THIS A BENTUK x(n)-x(n-3) :')
+    print('From : x = ', x)
+    print('       n = ', n)
+    pyogclr.print_pass('opening figure . . .')
+    x1 = x[0] - x[0]
+    x2 = x[1] - x[1]
+    x3 = x[2] - x[2]
+    x4 = x[3] - x[3]
+    x5 = x[4] - x[4]
+    x6 = x[5] - x[5]
+    xberubah = x1, x2, x3, x4, x5, x6
+    n1 = n[0] - (n[0]-3)
+    n2 = n[1] - (n[1]-3)
+    n3 = n[2] - (n[2]-3)
+    n4 = n[3] - (n[3]-3)
+    n5 = n[4] - (n[4]-3)
+    n6 = n[5] - (n[5]-3)
+    nberubah = n1, n2, n3, n4, n5, n6
+    # xberubah = -x
+    # nberubah = n - 3
+    grafis(nberubah, xberubah)
+
+
+# function that returns dy/dt
 def model(y, t):
     k = 0.3
     dydt = -k * y
     return dydt
 
 
+def differentialEquation():
+    print('\n ')
+    print('model: Function name that returns derivative values at requested y and t values as dydt=model(y, t)')
+    print('y0: Initial conditions of the differential states')
+    print('t: Time points at which the solution should be reported. Additional internal points are often calculated to maintain accuracy of the solution but are not reported.')
+    pyogclr.print_pass('opening figure . . .')
+    print('From : x = ', x)
+    print('       n = ', n)
+    # initial condition
+    # for x(n) = { 1 5 0 3 9 9}
+    y0 = x
+
+    # time points
+    # arr[0:5]
+    t = np.linspace(n[0], n[5])
+
+    # solve ODE
+    y = odeint(model, y0, t)
+
+    # plot results
+    plt.plot(t, y)
+
+    plt.title('Differential Equation')
+    plt.grid(True)
+    plt.ylabel('y(t)')
+    plt.xlabel('time')
+
+    plt.show()
+    yogiAsk()
+
+
 def bentukFxn():
     print('\n\n')
     pyogclr.print_pass('            THIS A BENTUK FUNGSI x(n) :')
-    print('\t\t\t x(n) = { n | ', n[0], ' <= n <= ', n[5], ', n bil. ganjil }')
+    print('\n\t\t\t x(n) = { n | ', n[0],
+          ' <= n <= ', n[5], ', n bil. ganjil }')
     yogiAsk()
 
 
 def tabularForm():
     print('\n\n')
     pyogclr.print_pass('            THIS A TABULAR FORM :')
+    print('\t\t WHERE ')
+    print('\t\t\t x(n) = { d d m m y y }')
     print('\t\t\t x(n) = {', x[0], x[1], x[2], x[3], x[4], x[5], '}')
     yogiAsk()
 
@@ -158,15 +272,20 @@ def grafis(n, x):
     markerline, stemlines, baseline = plt.stem(n, x, '-.')
     # setting property of baseline with color red and linewidth 2
     plt.setp(baseline, color='r', linewidth=2)
+
     xn = 'grafis x(n) = { '
     xn1 = x
     xn2 = ' }'
     xnh = xn, xn1, xn2
 
+    nilFrom = n[0]
+    nilEnd = n[5]
+    label = ('N = [ ', nilFrom, ':', nilEnd, ' ]')
+
     plt.title(xnh)
     plt.grid(True)
-    plt.ylabel('x(n)')
-    plt.xlabel('n = [0:5]')
+    plt.ylabel('Grafis Discrete Signal')
+    plt.xlabel(label)
     # plt.yscale('linear')
     plt.show()
     yogiAsk()
@@ -174,7 +293,8 @@ def grafis(n, x):
 
 def yoGrafis():
     pyogclr.print_pass('opening figure . . .')
-
+    print('From : x = ', x)
+    print('       n = ', n)
     grafis(n, x)
 
 
@@ -210,6 +330,7 @@ def yoFourier():
 
 
 def ioGrafis():
+    print("panjang isi variable x dan n adalah 6 / arr[6] ")
     print("Masukan Nilai x : ")
 
     # x = [int(i) for i in input().split()]
