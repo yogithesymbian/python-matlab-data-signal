ffdb5039bcb131301c0afce4879c854414a1404c
yogithesymbian <yogirenbox33@gmail.com>
2019-05-24 01:45:25 -0400

    z-tranform

---
+++ ztransform.m
@@ -0,0 +1,13 @@
+%% Z transform
+clc
+close all % close all
+clear all % clear all variable
+x = [ 1 5 0 3 9 9 ];
+b = 0;
+% calculating length of an birth of date
+n = length(x);
+y = sym('z');
+for i = 1:n
+    b = b+x(i)*y^(1-i);
+end
+display(b);
