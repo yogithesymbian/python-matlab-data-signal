740ef780e58087c771f2ee5e1b574bd6190db443
yogithesymbian <yogirenbox33@gmail.com>
2019-05-24 02:45:51 -0400

    update-transform

--- fouriertransform1.m
+++ fouriertransform1.m
@@ -1,3 +1,8 @@
-syms t v w x;
-f = exp(-x^2);
-fw=fourier(f);
+// syms t v w x;
+// x from birth of my date
+// f = exp(-x^2);
+// fw=fourier(f);
+
+syms t w;
+x = -exp(-t)*heaviside(t)+3*dirac(t);
+fw = fourier(x)
